b3ac020ca3c316851aa9bf772bf331bc717014ca
T.J <Tamimjabr@github.com>
2020-08-16 21:20:09 +0200

    adding a test product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,14 @@
 import React from "react"
 import "./Product.css"
+import cats from"../images/cats.jpg"
 
 function Product(){
     return(
         <div id="product"> 
-        product       
-        
+        <img src={cats} alt="missing"/>     
+        <p>art-nr:7979 <br/>
+        <b>Pris: 299 kr</b></p>
+        <button>Köp</button>      
         </div>
     )
 }
