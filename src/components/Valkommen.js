fc3fa8b32baa58e8493071d3b0ed32e4c8bbcc09
T.J <Tamimjabr@github.com>
2020-08-26 21:47:28 +0200

    adding vid

--- src/components/Valkommen.js
+++ src/components/Valkommen.js
@@ -1,12 +1,12 @@
 import React from "react"
 import "./Valkommen.css"
-import MELLINUS from "../video/MELLINUS.mp4"
+import valkommeVid from "../video/valkommeVid"
 
 
 function Valkommen(){
     return(
         <div id="valkommen" >
-            <video  src={MELLINUS} autoPlay> Your browser does not support this video</video>           
+            <video  src={valkommeVid} autoPlay> Your browser does not support this video</video>           
         </div>
     )
      
