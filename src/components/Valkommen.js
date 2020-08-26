40e98dc9af9e486e3e9a8bc14c8e216e4d15d5eb
T.J <Tamimjabr@github.com>
2020-08-26 22:25:35 +0200

    change the video to gif image

--- src/components/Valkommen.js
+++ src/components/Valkommen.js
@@ -1,15 +1,20 @@
 import React from "react"
 import "./Valkommen.css"
-import valkommeVid from "../video/valkommeVid.mp4"
+/*import valkommeVid from "../video/valkommeVid.mp4"*/
+import welcome from "../images/welcome.gif"
 
 
 function Valkommen(){
     return(
         <div id="valkommen" >
-            <video  src={valkommeVid} autoPlay> Your browser does not support this video</video>           
+            <img src={welcome} alt="welcome"/>
+
+            
         </div>
     )
      
     
 }
 export default Valkommen
+/*alternativ to use video */
+/*<video  src={valkommeVid} autoPlay> Your browser does not support this video</video>           */
