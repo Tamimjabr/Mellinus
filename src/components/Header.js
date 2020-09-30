2059b2be757f560c95e4441b85cf7b3bad6c8201
Tamim Jabr <tj222kg@student.lnu.se>
2020-09-30 20:59:14 +0200

    add search icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,29 @@
-import React from "react"
-import "./Header.css"
-import {AiOutlineShoppingCart} from "react-icons/ai"
-import {GiPresent} from "react-icons/gi"
-import { Link } from "react-router-dom"
+import React from "react";
+import "./Header.css";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { GiPresent } from "react-icons/gi";
+import { Link } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
 
-function Header(){
-    return(
-        <haeder id="header">
-             <Link to="/" id="mellinus">
-             <p>Mellinus<br/><GiPresent/> Present</p>
-            </Link>
-            <div  id="search">
-             <input name="search" type="text" placeholder="Sök bland våra produkter"/>   
-            </div>
-            <div>
-            <AiOutlineShoppingCart id="shoppingCart"/>
-            </div>      
-        
-        </haeder>
-    )
-     
-    
+function Header() {
+  return (
+    <haeder id="header">
+      <Link to="/" id="mellinus">
+        <p>
+          Mellinus
+          <br />
+          <GiPresent /> Present
+        </p>
+      </Link>
+
+      <div id="searchAndCart">
+        <div id="search">
+          <BsSearch />
+          <input name="search" type="text" placeholder="Sök" />
+        </div>
+        <AiOutlineShoppingCart id="shoppingCart" />
+      </div>
+    </haeder>
+  );
 }
-export default Header
+export default Header;
